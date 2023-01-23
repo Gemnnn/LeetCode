6c5f546cfbb733f188ce059d5963ae94e1dc6751
Branden Min <brandenmin@gmail.com>
2023-01-22 21:58:57 -0500

    21. Merge Two Sorted Lists

---
+++ 21_Javascript_MergeTwo SortedLists/21_MergeTwo SortedLists.js
@@ -0,0 +1,47 @@
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} list1
+ * @param {ListNode} list2
+ * @return {ListNode}
+ */
+var mergeTwoLists = function(list1, list2) {
+    var result = new ListNode(0);
+
+    if (list1 == null & list2 == null) {
+        return null;
+    }
+    if (list1 == null) {
+        return list2;
+    }
+    if (list2 == null) {
+        return list1;
+    }
+
+    var current = result;
+    while (list1 != null && list2 != null){
+        if (list1.val <= list2.val){
+            current.next = list1;
+            list1 = list1.next;
+        }
+        else {
+            current.next = list2;
+            list2 = list2.next;
+        }
+        current = current.next;
+    }
+
+    if (list1 != null) {
+        current.next = list1;
+    }
+    if (list2 != null){
+        current.next = list2;
+    }
+
+    return result.next;
+};
