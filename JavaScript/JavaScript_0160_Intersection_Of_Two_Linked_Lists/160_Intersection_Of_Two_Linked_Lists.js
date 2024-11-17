938ac383cf58c3f887c8f57911d08b4602c5cec8
Branden Min <brandenmin@gmail.com>
2024-11-17 18:56:07 -0500

    Update 160_Intersection_Of_Two_Linked_Lists.js

--- JavaScript/JavaScript_0160_Intersection_Of_Two_Linked_Lists/160_Intersection_Of_Two_Linked_Lists.js
+++ JavaScript/JavaScript_0160_Intersection_Of_Two_Linked_Lists/160_Intersection_Of_Two_Linked_Lists.js
@@ -35,3 +35,45 @@ var getIntersectionNode = function(headA, headB) {
 
 // Time Complexity: O(m×n)
 // Space Complexity: O(1)
+
+
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val) {
+ *     this.val = val;
+ *     this.next = null;
+ * }
+ */
+
+/**
+ * @param {ListNode} headA
+ * @param {ListNode} headB
+ * @return {ListNode}
+ */
+var getIntersectionNode = function(headA, headB) {
+    if (!headA || !headB) {
+        return null; 
+    }
+
+    let currentA = headA;
+    let currentB = headB;
+
+    while (currentA !== currentB){
+        if(currentA !== null){
+            currentA = currentA.next;
+        } else{
+            currentA = headB;
+        }
+
+        if(currentB !== null){
+            currentB = currentB.next;
+        } else{
+            currentB = headA;
+        }
+    }
+
+    return currentA;
+};
+
+// Time Complexity: O(m+n)
+// Space Complexity: O(1)
